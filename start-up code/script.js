3ccf84d2f294d1dccb743ae877ab95838e64fa85
MHS Game Development Club <115768619+mshgamedevelopmentclub@users.noreply.github.com>
2022-11-29 07:53:22 -0800

    Create script.js

---
+++ start-up code/script.js
@@ -0,0 +1,12 @@
+<!--1. Prompt the user to enter a maximum number and save that to a variable-->
+<!--2. parseInt the prompt so that it accepts only integers-->
+<!--3. Generate a random number and save that to a variable called targetNum. Times that number by the user input-->
+<!--4. While the user does not put in a valid input, prompt them again to enter a valid input. Save that to a variable called targetNum-->
+<!--5. Prompt them to enter their first guess. Save that to a variable called guess-->
+<!--6. If the guess is greater than the targetNum, prompt the user "Too high! Enter a new guess:"-->
+<!--7. Otherwise, prompt the user "Too low! Enter a new guess:""-->
+<!--8. if guess is ever equal to "q", break the code-->
+<!--9. if guess is equal to "q" or "quit" after a guess, console.log "OK, YOU QUIT!"-->
+<!--10. Otherwise console.log "CONGRATS YOU WIN!"-->
+
+<!--Optional:  Make a variable of the number of attemps and set that to equal 1. Then make a string template literal (``) to tell the user the number of attemps the user did to guess the number. The number needs to be updated over time (+=)-->
