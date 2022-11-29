6bb51d969b9c36ec32c6b11cd2b66208d59d5655
MHS Game Development Club <115768619+mshgamedevelopmentclub@users.noreply.github.com>
2022-11-28 22:29:28 -0800

    Create script.js

---
+++ solution/script.js
@@ -0,0 +1,26 @@
+let maximum = parseInt(prompt("Enter a maximum number!"));
+while (!maximum) {
+  maximum = parseInt("Enter a maximum number!");
+}
+
+const targetNum = Math.floor(Math.random() * maximum) * 1;
+
+let guess = prompt("Enter in your first guess!");
+let attempts = 1;
+
+while (parseInt(guess) !== targetNum) {
+  if (guess === "q") break;
+  attempts++;
+  if(guess > targetNum) {
+    guess = parseInt(prompt("Too high! Enter a new guess!"));
+  } else {
+    guess = parseInt(prompt("Too low! Enter a new guess!"));
+  }
+}
+if(guess === "q") {
+  console.log("OK, YOU QUIT!");
+} else {
+  console.log("CONGRATS YOU WIN!");
+  console.log(`You got it! It took you ${attempts} guesses`);
+}
+
